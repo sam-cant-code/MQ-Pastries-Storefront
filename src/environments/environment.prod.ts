123fa4ed4c30afda60aaa882e30324806e00028f
sam-cant-code <135305742+sam-cant-code@users.noreply.github.com>
2026-08-01 17:54:32 +0530

    correct testkey razorpay

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: '/api',
-  razorpayKey: 'rzp_test_TJKVZnZlWckIuW' // Replace with production key once razor pay sees that trasaction is working
+  razorpayKey: 'rzp_test_TKVDBnAR2RWVLW' // Replace with production key once razor pay sees that trasaction is working
 };
